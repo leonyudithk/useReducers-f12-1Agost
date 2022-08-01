295ceb76c4ef8a33e01590d3645fb54061b1f67e
yudithkleon <leonyudithk@gmail.com>
2022-08-01 12:58:50 -0500

    cambios en el navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import AppRouter from './routers/AppRouter';
-import { ChakraProvider } from '@chakra-ui/react';
+
 
 
 const App = () => {
   return (
-    <ChakraProvider>
+    
              <AppRouter/>
-    </ChakraProvider>
+
   );
 };
 
